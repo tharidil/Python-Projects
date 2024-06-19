9e09fc9966ba1214436ee88b9510b2f54a6bfbb5
tharidil <tharindu.dilhan.w@gmail.com>
2024-06-19 18:44:58 +0300

    Add files via upload

---
+++ Boring game.py
@@ -0,0 +1,6 @@
+name = input("Please enter your name: ")
+print("Welcome",name,"to the boring game.")
+
+q1 = input("You are going to go to bed. It is 9 pm. Would you set an alarm? (yes/no): ").lower()
+if q1 == "yes":
+    q12 =input("What is the time you set the alarm? (5am/6am/7am) ")
