9794c0b057f6a74cfccba3899718df3fbcbdf960
tharidil <tharindu.dilhan.w@gmail.com>
2024-06-23 14:59:09 +0300

    Update Dice game.py

--- Dice game.py
+++ Dice game.py
@@ -1,64 +1,52 @@
 import random
 
-#def roll():
-    
-
+def roll():
+    min=1
+    max=6
+    roll=random.randint(min,max)
+    return roll
 
 while True:
     players = input("How many players? (2-4): ")
     if players.isdigit():
         players=int(players)
-        if 2<=players<=6:
-            current_score=0
-            max_score=50
-    
-            while True:
-                Game_play=input("Would you like to roll the dice? (y/n): ")
-                if Game_play.lower== "y":
-                    value=roll()
-
-                    if value!= 1:
-                        current_score += value
-                        if current_score==max_score:
-                            break
-                    else:
-                        print("You rolled a",value,"!")
-                        current_score = 0
-                        print("You got a score of",current_score)
-                        break
-                elif Game_play.lower== "n":
-                    quit()
-                else:
-                    print("Enter a valid answer")
-                    
-                
+        if 2<=players<=4:
+            break            
         else:
             print("Number of players should be 2 to 4")    
     else:
-        Print("Please enter a valid number")
+        print("Please enter a valid number")
         continue
-    
-    '''current_score=0
-    max_score=50
-    
-    while True:
-        Game_play=input("Would you like to roll the dice? (y/n): ")
-        if Game_play.lower==("y"):
-            roll = random.randint(1,6)
-            print("You got ",roll,"!")
+ 
+max_score=20
+player_scores=[0 for i in range(players)]
+print(player_scores)
 
-            if roll!=(1):
-                current_score = current_score+roll
-                if current_score==max_score:
+    
+while max(player_scores) < max_score:
+    for player_index in range(players):
+        print("Turn of player",player_index+1)
+        print("Your currrent score is", player_scores[player_index])
+
+        while True:
+            Game_play=input("Would you like to roll the dice? (y/n): ")
+            if Game_play.lower() == 'y':
+                value=roll()
+
+                if value!= 1:
+                    player_scores[player_index] += value
+                    print("You rolled a",value,"!")
+                    print("Current score: ",player_scores[player_index])                 
+                                    
+                else:
+                    print("You rolled a",value,"!")
+                    player_scores[player_index] = 0
+                    print("Next player please. Your new score: ",player_scores[player_index])
                     break
+                
+            elif Game_play.lower() == "n":
+                break
             else:
-                current_score = 0
-        elif Game_play.lower==("n"):
-            quit()
-        else:
-            Print("Enter a valid answer")
-            
-        print("You got",current_score)
-        break'''
+                print("Enter a valid answer")
         
-
+print("Player",player_index+1,"won !!!")
