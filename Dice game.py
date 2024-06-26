a307a31423c1f2f895ae2fed0a48b674f2cc3b19
tharidil <tharindu.dilhan.w@gmail.com>
2024-06-26 20:11:38 +0300

    Update Dice game.py

--- Dice game.py
+++ Dice game.py
@@ -13,7 +13,7 @@ while True:
         if 2<=players<=4:
             break            
         else:
-            print("Number of players should be 2 to 4")    
+            print("Number of players should be 2 to 4.")    
     else:
         print("Please enter a valid number")
         continue
@@ -26,27 +26,31 @@ print(player_scores)
 while max(player_scores) < max_score:
     for player_index in range(players):
         print("Turn of player",player_index+1)
-        print("Your currrent score is", player_scores[player_index])
-
+        print("Your currrent score is", player_scores[player_index],".")
+        current_score=0
         while True:
             Game_play=input("Would you like to roll the dice? (y/n): ")
-            if Game_play.lower() == 'y':
-                value=roll()
-
-                if value!= 1:
-                    player_scores[player_index] += value
-                    print("You rolled a",value,"!")
-                    print("Current score: ",player_scores[player_index])                 
-                                    
-                else:
-                    print("You rolled a",value,"!")
-                    player_scores[player_index] = 0
-                    print("Next player please. Your new score: ",player_scores[player_index])
-                    break
-                
-            elif Game_play.lower() == "n":
+            if Game_play.lower() == 'n':
                 break
+            if Game_play.lower() != 'y':
+                print("Next player please. You turned down the turn.")
+                break
+            value=roll()
+
+            if value == 1:
+                current_score -= value
+                print("You rolled a",value,"! and the current score is ",current_score,".")
+                print("Next player please.")
+                break                 
+                                    
             else:
-                print("Enter a valid answer")
+                print("You rolled a",value,"!")
+                current_score += value
+            print( "Your score as of now: ",current_score)
+
+        player_scores[player_index]+=current_score  
+        #print("Your total score is", player_scores[player_index],".")    
+                
         
-print("Player",player_index+1,"won !!!")
+            
+print("Player",player_index+1,"won with a score of",str(max(player_scores))," !!!")
