55bf35d7c249028cc464afb02c7ac24762d9aab4
tharidil <tharindu.dilhan.w@gmail.com>
2024-06-30 16:38:42 +0300

    Add files via upload

---
+++ randommadlib.py
@@ -0,0 +1,15 @@
+
+import random
+
+name1 = input("What is your name: ")
+name2 = input("What is your friend's name: ")
+verb1 = input("What do yoi like to do in your free time? ")
+
+
+madlib = f"My name is {name1} and my mission is to find out {name2},\
+because he used to be my partner in crime but he is missing now. We were {verb1}\
+the last time I met him hence I cannot tell it to anyone."
+
+
+    
+print(madlib)
